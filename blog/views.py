e7f27f0dc7f29b62c73d0b35a09d05bd7a068c00
SRISHANTH P M <srishanth232007@gmail.com>
2025-03-13 21:16:33 +0530

    Connecting MySQL Database, Creating Model, Creating Migrations, Inserting Data, Getting Posts Data and Getting Data by Id

--- blog/views.py
+++ blog/views.py
@@ -2,24 +2,35 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 import logging
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {'id' : 1, 'title' : 'Post 1', 'content' : 'Content of Post 1'},
-    {'id' : 2, 'title' : 'Post 2', 'content' : 'Content of Post 2'},
-    {'id' : 3, 'title' : 'Post 3', 'content' : 'Content of Post 3'},
-    {'id' : 4, 'title' : 'Post 4', 'content' : 'Content of Post 4'},
-]
+# Getting data which is hard coded
+# posts = [
+#     {'id' : 1, 'title' : 'Post 1', 'content' : 'Content of Post 1'},
+#     {'id' : 2, 'title' : 'Post 2', 'content' : 'Content of Post 2'},
+#     {'id' : 3, 'title' : 'Post 3', 'content' : 'Content of Post 3'},
+#     {'id' : 4, 'title' : 'Post 4', 'content' : 'Content of Post 4'},
+# ]
 
 def index(request):
     blog_title = "Latest Posts"
+
+    # Getting data from Post model
+    posts = Post.objects.all()
     return render(request, "blog/index.html", {'blog_title' : blog_title, 'posts' : posts})
 
 def detail(request, post_id):
-    post = next((item for item in posts if item['id'] == int(post_id)), None)
+    # Getting data from hard coded list 
+    # post = next((item for item in posts if item['id'] == int(post_id)), None)
+
+    # Getting data from Post model using post_id
+    post = Post.objects.get(pk = post_id)
+
     # logger = logging.getLogger("TESTING")
     # logger.debug(f"Post variable is {post}")
+
     return render(request, "blog/detail.html", {'post' : post})
 
 def old_url_redirect(request):
